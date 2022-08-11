357ca5498bfd1e26d2299c412f6c1c8be07d5f2f
Rachel <restartstudy2022@gmail.com>
2022-08-12 00:13:04 +0800

    菜單畫面與filter完成,購物車清單畫面完成80%

--- js/fooditem.js
+++ js/fooditem.js
@@ -80,6 +80,206 @@ const foodItem= [
         img: 'images/noodle/noodle-6.png',
         quantity: 1
     },
+    {
+        id: 10,
+        name: '義大利麵001',
+        category : '義大利麵',
+        rating : 4.3,
+        price: 11,
+        img: 'images/pasta/pasta-01.png',
+        quantity: 1
+    },
+    {
+        id: 11,
+        name: '義大利麵002',
+        category : '義大利麵',
+        rating : 4.3,
+        price: 10,
+        img: 'images/pasta/pasta-02.png',
+        quantity: 1
+    },
+    {
+        id: 12,
+        name: '義大利麵003',
+        category : '義大利麵',
+        rating : 4.3,
+        price: 14,
+        img: 'images/pasta/pasta-03.png',
+        quantity: 1
+    },
+    {
+        id: 13,
+        name: '義大利麵004',
+        category : '義大利麵',
+        rating : 4.3,
+        price: 12,
+        img: 'images/pasta/pasta-04.png',
+        quantity: 1
+    },
+    {
+        id: 14,
+        name: '義大利麵005',
+        category : '義大利麵',
+        rating : 4.3,
+        price: 17,
+        img: 'images/pasta/pasta-05.png',
+        quantity: 1
+    },
+    {
+        id: 15,
+        name: '義大利麵006',
+        category : '義大利麵',
+        rating : 4.3,
+        price: 20,
+        img: 'images/pasta/pasta-06.png',
+        quantity: 1
+    },
+    {
+        id: 16,
+        name: '義大利麵007',
+        category : '義大利麵',
+        rating : 4.3,
+        price: 15,
+        img: 'images/pasta/pasta-07.png',
+        quantity: 1
+    
+    },
+    {
+        id: 17,
+        name: '義大利麵008',
+        category : '義大利麵',
+        rating : 4.3,
+        price: 10,
+        img: 'images/pasta/pasta-08.png',
+        quantity: 1
+    },
+    {
+        id: 18,
+        name: '義大利麵009',
+        category : '義大利麵',
+        rating : 4.3,
+        price: 15,
+        img: 'images/pasta/pasta-09.png',
+        quantity: 1    
+    },
+    {
+        id: 19,
+        name: '披薩001',
+        category : '披薩',
+        rating : 4.3,
+        price: 12,
+        img: 'images/pizza/pizza-01.png',
+        quantity: 1
+    
+    },
+    {
+        id: 20,
+        name: '披薩002',
+        category : '披薩',
+        rating : 4.3,
+        price: 8,
+        img: 'images/pizza/pizza-02.png',
+        quantity: 1
+    },
+    {
+        id: 21,
+        name: '披薩003',
+        category : '披薩',
+        rating : 4.3,
+        price: 7,
+        img: 'images/pizza/pizza-03.png',
+        quantity: 1
+    },
+    {
+        id: 22,
+        name: '披薩004',
+        category : '披薩',
+        rating : 4.3,
+        price: 5,
+        img: 'images/pizza/pizza-04.png',
+        quantity: 1
+    },
+    {
+        id: 23,
+        name: '披薩005',
+        category : '披薩',
+        rating : 4.3,
+        price: 8,
+        img: 'images/pizza/pizza-05.png',
+        quantity: 1
+    },
+    {
+        id: 24,
+        name: '披薩006',
+        category : '披薩',
+        rating : 4.3,
+        price: 7,
+        img: 'images/pizza/pizza-06.png',
+        quantity: 1
+    },
+    {
+        id: 25,
+        name: '披薩007',
+        category : '披薩',
+        rating : 4.3,
+        price: 5,
+        img: 'images/pizza/pizza-07.png',
+        quantity: 1
+    },
+    {
+        id: 26,
+        name: '披薩008',
+        category : '披薩',
+        rating : 4.3,
+        price: 7,
+        img: 'images/pizza/pizza-08.png',
+        quantity: 1
+    },
+    {
+        id: 27,
+        name: '披薩009',
+        category : '披薩',
+        rating : 4.3,
+        price: 10,
+        img: 'images/pizza/pizza-09.png',
+        quantity: 1
+    },
+    {
+        id: 28,
+        name: '甜點001',
+        category : '甜點',
+        rating : 4.3,
+        price: 4,
+        img: 'images/sweet/sweet-01.png',
+        quantity: 1
+    },
+    {
+        id: 29,
+        name: '甜點002',
+        category : '甜點',
+        rating : 4.3,
+        price: 4,
+        img: 'images/sweet/sweet-02.png',
+        quantity: 1
+    },
+    {
+        id: 30,
+        name: '甜點003',
+        category : '甜點',
+        rating : 4.3,
+        price: 8,
+        img: 'images/sweet/sweet-03.png',
+        quantity: 1
+    },
+    {
+        id: 31,
+        name: '甜點004',
+        category : '甜點',
+        rating : 4.3,
+        price: 7,
+        img: 'images/sweet/sweet-04.png',
+        quantity: 1
+    },
 ]
 
 export {foodItem};
--- js/script.js
+++ js/script.js
@@ -1,2 +1,213 @@
 import { foodItem } from './fooditem.js'
 console.log(foodItem);
+
+function displayItem(){
+    var fruit = document.querySelector('#fruit');
+    var noodle = document.querySelector('#noodle');
+    var pasta = document.querySelector('#pasta');
+    var pizza = document.querySelector('#pizza');
+    var sweet = document.querySelector('#sweet');
+
+    const fruitData = foodItem.filter(item => item.category =='水果');
+    console.log(fruitData);
+    const noodleData = foodItem.filter(item => item.category =='拉麵');
+    console.log(noodleData);
+    const pastaData = foodItem.filter(item => item.category =='義大利麵');
+    console.log(pastaData);
+    const pizzaData = foodItem.filter(item => item.category =='披薩');
+    console.log(pizzaData);
+    const sweetData = foodItem.filter(item => item.category =='甜點');
+    console.log(sweetData);
+    
+    fruitData.map(item => {
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id','item-card');
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText=' '+ item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img=document.createElement('img');
+        img.src=item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id', 'item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= '價錢 NT$ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        fruit.appendChild(itemCard);
+    })
+
+    noodleData.map(item =>{
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id','item-card');
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img = document.createElement('img');
+        img.src=item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText=item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText = '價錢 NT$ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        noodle.appendChild(itemCard);
+    })
+
+    pastaData.map(item =>{
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id','item-card');
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img = document.createElement('img');
+        img.src=item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText=item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText = '價錢 NT$ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        pasta.appendChild(itemCard);
+    })
+
+    pizzaData.map(item =>{
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id','item-card');
+
+        var cardTop=document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img = document.createElement('img');
+        img.src = item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText=item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= '價錢 NT$ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        pizza.appendChild(itemCard);
+    })
+
+    sweetData.map(item =>{
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id','item-card');
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class','fa fa-star')
+        star.setAttribute('id','rating');
+        star.innerText = ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id);
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+        var img = document.createElement('img');
+        img.src = item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText = item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerHTML = '價錢 NT$ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        sweet.appendChild(itemCard);
+    })
+}
+
+displayItem();
